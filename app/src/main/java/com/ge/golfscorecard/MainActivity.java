b97412e05af6c0ebbf51d8f4707fb45f0bf26c31
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-06 10:19:02 -0600

    Implemented shared preferences for data persistence.

--- app/src/main/java/com/ge/golfscorecard/MainActivity.java
+++ app/src/main/java/com/ge/golfscorecard/MainActivity.java
@@ -1,25 +1,46 @@
 package com.ge.golfscorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private static final String PREFS_FILE = "com.ge.golfScorecard.preferences";
+    private final String KEY_STROKECOUNT = "key_strokecount";
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
         // Initialize holes.
         int strokeCount = 0;
         for (int i = 0; i < mHoles.length; i++){
+            strokeCount = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
             mHoles[i] = new Hole("Hole " + (i + 1) + " :", strokeCount);
         }
 
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i < mHoles.length; i++){
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getStrokeCount());
+        }
+
+        mEditor.apply();
     }
 }
