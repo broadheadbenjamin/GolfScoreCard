7f8ab6da8280f308be5cef5a2a06d05e1f106463
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-06 09:42:36 -0600

    Create hole class and initialized hole array.

--- app/src/main/java/com/ge/golfscorecard/MainActivity.java
+++ app/src/main/java/com/ge/golfscorecard/MainActivity.java
@@ -5,10 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
+    private Hole[] mHoles = new Hole[18];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize holes.
+        int holeCount = 1;
+        int strokeCount = 0;
+        for (Hole hole: mHoles) {
+            hole = new Hole("Hole" + holeCount + " :", strokeCount);
+            holeCount++;
+        }
     }
 }
